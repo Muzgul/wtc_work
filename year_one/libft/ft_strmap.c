6ee6f4c8a4455dd85de8032337826790ca086fdd
Murray MACDONALD <mmacdona@e5r10p8.jnb.42.fr>
2017-05-24 12:27:58 +0200

    Progress

--- year_one/libft/ft_strmap.c
+++ year_one/libft/ft_strmap.c
@@ -6,7 +6,7 @@
 /*   By: mmacdona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/24 11:18:14 by mmacdona          #+#    #+#             */
-/*   Updated: 2017/05/24 11:22:52 by mmacdona         ###   ########.fr       */
+/*   Updated: 2017/05/24 11:25:48 by mmacdona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,19 @@ char	*ft_strmap(char const *s, char (*f)(char))
 	int i;
 	char *str;
 
+	str = NULL;
 	if (f != NULL && s != NULL)
 	{
 		str = ft_strnew(ft_strlen(s));
-
+		if (str == NULL)
+		{
+			i = 0;
+			while (s[i] != '\0')
+			{
+				str[i] = (*f)(s[i]);
+				i++;
+			}
+		}
 	}
+	return (str);
 }
