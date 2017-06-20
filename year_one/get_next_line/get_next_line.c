3860890b59a94b7263e1e66d5a9da61822738f40
Murray MacDonald <mmacdona@Plumb.local>
2017-06-20 23:23:02 +0200

    Worked on get_next_line

--- year_one/get_next_line/get_next_line.c
+++ year_one/get_next_line/get_next_line.c
@@ -1,19 +1,20 @@
-
 #include "get_next_line.h"
 #include "libft.h"
 
 int		get_next_line(const in fd, char **line)
 {
-	static list HEAD;
+	static s_list HEAD;
 	char buff[BUFF_SIZE];
 	int count;
-	while (list_contains('/n') == 0)
+
+	while (ft_strsearch(buff, '/n') == 0)
 	{
 		read(buff);
-		count += count_chars(buff);
-		list_add(buff);
-		clear(buff);
+		count += ft_strlen(buff);
+		ft_lstadd(&HEAD, ft_lstnew((void *)buff, ft_strlen(buff)));
+		ft_strclr(buff);
 	}
+	//TODO Fix for when buff includes \n and code make_line function; Include lseek functionality; Include EOF stopper;
 	line = make_line();
 	reset_read;
 	return (list);
--- year_one/libft/libft.h
+++ year_one/libft/libft.h
@@ -16,6 +16,17 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct	s_list
+{
+	void		*content;
+	size_t		content_size;
+	struct s_list	*next;
+}			t_list;
+
+t_list	*ft_lstnew(void const *content, size_t content_size);
+void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
+void	_ftlstdel(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstadd(t_list **alst, t_list *new);
 size_t		ft_strsearch(const char *str, char c);
 char	*ft_array_up(char *ap, size_t size, size_t n);
 size_t		ft_arrlen(const void *arr);
