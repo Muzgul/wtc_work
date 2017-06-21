f94726dfbc5c9531bba4f222eb6e02e4eabc2d9d
Murray MACDONALD <mmacdona@e4r7p2.jnb.42.fr>
2017-06-21 14:17:17 +0200

    save changes finished get_next_line

--- year_one/get_next_line/get_next_line.c
+++ year_one/get_next_line/get_next_line.c
@@ -1,21 +1,57 @@
 #include "get_next_line.h"
 #include "libft.h"
 
-int		get_next_line(const in fd, char **line)
+int		line_end()
+{
+	if (ft_strsearch(HEAD->content, '\n') != 0 || ft_strsearch(HEAD->content, -1) != 0)
+		return (1);
+	return (0);
+}
+
+void	read_to_list(const int fd)
+{
+	char buff[BUFF_SIZE];
+	t_list *new;
+
+	read(fd, buff, BUFF_SIZE);
+	if (ft_strsearch(buff, '\n') != 0)
+		new = ft_lstnew((void*)buff, ft_strsearch(buff, '\n'));
+	else
+	{
+		if (ft_strsearch(buff, -1) != 0)
+			new = ft_lstnew((void*)buff, ft_strsearch(buff, -1));
+		else
+			new = ft_lstnew((void*)buff, ft_strlen(buff)));
+	}
+	count += new->content_size;
+	ft_lstadd(&HEAD, new);
+}
+
+void	make_line(char **line)
+{
+	t_list *current;
+
+	*line = ft_strnew(count);
+	current = HEAD;
+	while (current != NULL)
+	{
+		*list = ft_strjoin(current->content, *list);
+		current = current->next;
+	}
+}
+
+int		get_next_line(const int fd, char **line)
 {
 	static s_list HEAD;
-	char buff[BUFF_SIZE];
-	int count;
+	static size_t count;
 
-	while (ft_strsearch(buff, '/n') == 0)
+	count = 0;
+	while (line_end() == 0)
 	{
-		read(buff);
-		count += ft_strlen(buff);
-		ft_lstadd(&HEAD, ft_lstnew((void *)buff, ft_strlen(buff)));
-		ft_strclr(buff);
+		read_to_list(fd);
 	}
-	//TODO Fix for when buff includes \n and code make_line function; Include lseek functionality; Include EOF stopper;
-	line = make_line();
-	reset_read;
+	
+	make_line();
+	lseek(fd, count, SEEK_SET);
 	return (list);
 }
