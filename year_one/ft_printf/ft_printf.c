b7a0b5e31c068826e62294a99a108029f390a66b
Murray MACDONALD <mmacdona@e5r4p11.wethinkcode.co.za>
2017-08-21 14:37:59 +0200

    start on printf

---
+++ year_one/ft_printf/ft_printf.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmacdona <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/21 13:45:38 by mmacdona          #+#    #+#             */
+/*   Updated: 2017/08/21 14:37:15 by mmacdona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+int		ft_printf(const char * restrict format, ...)
+{
+	char	*ptr;
+	va_list args;
+	va_start(args, format);
+
+	ptr = (char *)format;
+	while (*ptr != '\0')
+	{
+		if (*ptr == '%')
+		{
+			ptr++;
+			if (*ptr == 's')
+				ft_putstr(va_arg(args, char *));
+		}
+		else
+			ft_putchar(*ptr);
+		ptr++;
+	}
+	va_end(args);
+	return (0);
+}
