94830000120b93eb45d2919dc0ece03b710f476a
Murray MACDONALD <mmacdona@e5r5p6.wethinkcode.co.za>
2017-08-29 17:28:22 +0200

    Tue 29 Aug

--- year_one/ft_printf/ft_fs.c
+++ year_one/ft_printf/ft_fs.c
@@ -6,7 +6,7 @@
 /*   By: mmacdona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/24 14:44:33 by mmacdona          #+#    #+#             */
-/*   Updated: 2017/08/24 15:06:43 by mmacdona         ###   ########.fr       */
+/*   Updated: 2017/08/29 13:13:29 by mmacdona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,9 @@
 void	ft_s(va_list args)
 {
 	ft_putstr(va_arg(args, char *));
+}
+
+void	ft_d(va_list args)
+{
+	ft_putstr(ft_itoa(va_arg(args, int)));
 }
