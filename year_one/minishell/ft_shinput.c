2d3dfa09b26239cd2f858789bdbe4d684be842f4
Murray MACDONALD <mmacdona@e5r3p5.wethinkcode.co.za>
2017-09-18 13:17:57 +0200

    CLION integration;

--- year_one/minishell/ft_shinput.c
+++ year_one/minishell/ft_shinput.c
@@ -22,7 +22,10 @@ char	*ft_get_line()
 
 	str_len = 0;
 	head = NULL;
+	lst_ptr = NULL;
 	str = (char *)malloc(32 * sizeof(char));
+	if (str == NULL)
+		return (NULL);
 	while (read(STDIN_FILENO, &c, 1) > 0)
 	{
 		if (c == -1 || c == '\n')
@@ -32,8 +35,9 @@ char	*ft_get_line()
 		else
 		{
 			lst_ptr = ft_lstnew((void*)str, 32);
-			if (lst_ptr != NULL)
-				ft_lstadd(&head, lst_ptr);
+			if (lst_ptr == NULL)
+				return (NULL);
+			ft_lstadd(&head, lst_ptr);
 			ft_strclr(str);
 			str_len = 0;
 			str[str_len++] = c;
@@ -41,13 +45,25 @@ char	*ft_get_line()
 		if (c == '\0')
 		{
 			if (str_len > 0)
-				ft_lstadd(&head, ft_lstnew((void*)str, 32));
+			{
+				lst_ptr = ft_lstnew((void *)str, 32);
+				if (lst_ptr == NULL)
+					return (NULL);
+				ft_lstadd(&head, lst_ptr);
+			}
 			return (ft_lst_to_str(head));
 		}
 	}
+	if (str_len > 0)
+	{
+		lst_ptr = ft_lstnew((void *)str, 32);
+		if (lst_ptr == NULL)
+			return (NULL);
+		ft_lstadd(&head, lst_ptr);
+	}
 	if (head != NULL)
 		return (ft_lst_to_str(head));
-	return ("FAIL\n");
+	return (NULL);
 }
 
 int		ft_get_status(char *user_input)
@@ -66,10 +82,7 @@ int		ft_get_status(char *user_input)
 char	*ft_get_input(char *old_input)
 {
 	if (old_input != NULL)
-	{
 		free(old_input);
-		old_input = NULL;
-	}
 	old_input = ft_get_line();
 	if (old_input == NULL)
 	{
